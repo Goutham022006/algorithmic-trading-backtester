8eb57717d2f8e990e9f2fce5130a8f473ac02487
Goutham022006 <155623996+Goutham022006@users.noreply.github.com>
2026-09-16 18:21:15 +0530

    Remove generated project files

--- src/BuyAndHoldStrategy.cpp
+++
@@ -1,5 +0,0 @@
-#include "BuyAndHoldStrategy.h"
-
-Signal BuyAndHoldStrategy::generateSignal(const std::vector<MarketData>&, std::size_t index) const {
-    return index == 0 ? Signal::Buy : Signal::Hold;
-}
