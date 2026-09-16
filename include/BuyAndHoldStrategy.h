01c5373b3676a363b76be741d7267b0e189c918d
Goutham022006 <155623996+Goutham022006@users.noreply.github.com>
2026-09-16 18:20:40 +0530

    Remove generated project files

--- include/BuyAndHoldStrategy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Strategy.h"
-
-class BuyAndHoldStrategy : public Strategy {
-public:
-    Signal generateSignal(const std::vector<MarketData>& data, std::size_t index) const override;
-};
